8327dba201b20cdee13720380261bb3ad66942d5
Andrew Mayorov <encube.ul@gmail.com>
2022-05-16 11:43:32 +0300

    Drop accounter service dependency (#13)

--- apps/capi_woody_client/src/capi_woody_client.erl
+++ apps/capi_woody_client/src/capi_woody_client.erl
@@ -83,8 +83,6 @@ get_service_modname(reporting) ->
     {reporter_reports_thrift, 'Reporting'};
 get_service_modname(payouts) ->
     {payouts_payout_manager_thrift, 'PayoutManagement'};
-get_service_modname(accounter) ->
-    {dmsl_accounter_thrift, 'Accounter'};
 get_service_modname(webhook_manager) ->
     {dmsl_webhooker_thrift, 'WebhookManager'};
 get_service_modname(customer_management) ->
