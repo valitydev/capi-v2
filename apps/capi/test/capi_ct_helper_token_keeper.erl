031d6effbbb6f1c98e23b16e8f5c38cf0b0c8c93
Aleksey Kashapov <nanodirijabl@gmail.com>
2023-02-27 10:46:08 +0300

    Revert "TD-502: Enable usage of user_id and party_id from token (#26)" (#27)
    This reverts commit 2de9367561a511f0dc1448881201de48e9004c54.

--- apps/capi/test/capi_ct_helper_token_keeper.erl
+++ apps/capi/test/capi_ct_helper_token_keeper.erl
@@ -173,15 +173,13 @@ combine_metadata(#{} = FullMetadata) ->
 
 user_session_metadata() ->
     genlib_map:compact(#{
-        ?TK_META_PARTY_ID => ?PARTY_ID,
         ?TK_META_USER_ID => ?USER_ID,
         ?TK_META_USER_EMAIL => ?USER_EMAIL
     }).
 
 api_key_metadata() ->
     genlib_map:compact(#{
-        ?TK_META_PARTY_ID => ?PARTY_ID,
-        ?TK_META_USER_ID => ?USER_ID
+        ?TK_META_PARTY_ID => ?PARTY_ID
     }).
 
 consumer_metadata(Consumer) ->
