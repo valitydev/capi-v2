e3d8d50b40fb8ff6459caf6b3b9016f95c2f1183
Aleksey Kashapov <nanodirijabl@gmail.com>
2026-09-09 14:06:33 +0300

    Fixes most important code style violation

--- apps/capi/src/capi_handler_decoder_invoicing.erl
+++ apps/capi/src/capi_handler_decoder_invoicing.erl
@@ -351,6 +351,7 @@ decode_payment_operation_failure({failure, Failure}, Context) ->
 
 -spec format_failure(dmsl_domain_thrift:'OperationFailure'() | undefined) -> iolist().
 format_failure(Failure) -> lists:join($:, extract_failure_code(Failure)).
+
 extract_failure_code(undefined) -> [];
 extract_failure_code(#domain_Failure{code = Code, sub = Sub}) -> [Code | extract_failure_code(Sub)];
 extract_failure_code(#domain_SubFailure{code = Code, sub = Sub}) -> [Code | extract_failure_code(Sub)].
